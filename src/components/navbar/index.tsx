25b9ddf15e2364c9cc1846c6be3e14f4d97f2b99
yusuf <ysfyldz0077@gmail.com>
2024-09-27 21:18:14 +0300

    navbar fix

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -10,32 +10,26 @@ import { AnimatePresence, motion } from "framer-motion";
 
 const Navbar: React.FC = () => {
   const [open, setOpen] = useState<boolean>(false);
-  const menuRef = useRef<HTMLDivElement | null>(null);
-  const appleLogoRef = useRef<HTMLDivElement | null>(null);
+  const [activeMenuPath, setActiveMenuPath] = useState<string[]>([]);
 
-  // Menü dışında tıklamayı algılamak için useEffect kullanıyoruz
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
-      if (
-        menuRef.current &&
-        !menuRef.current.contains(event.target as Node) &&
-        appleLogoRef.current &&
-        !appleLogoRef.current.contains(event.target as Node)
-      ) {
+      const target = event.target as HTMLElement;
+
+      if (!target.className?.includes("menu")) {
         setOpen(false);
+        setActiveMenuPath([]);
       }
     };
 
-    // Tıklama olaylarını dinle
     document.addEventListener("mousedown", handleClickOutside);
 
-    // Cleanup: component unmount olduğunda event listener'ı kaldır
     return () => {
       document.removeEventListener("mousedown", handleClickOutside);
     };
   }, []);
 
-  // Yeni animasyon ayarları
+  // Animasyon ayarları
   const menuAnimation = {
     initial: { opacity: 0, scale: 0.9 },
     animate: { opacity: 1, scale: 1 },
@@ -43,16 +37,114 @@ const Navbar: React.FC = () => {
     transition: { type: "spring", stiffness: 250, damping: 20 },
   };
 
+  const renderMenuItems = (items: any[], path: string[]) => {
+    return items.map((item, index) => {
+      const currentPath = [...path, item.title].join("/");
+      const isLastItem = index === items.length - 1; // Son öğeyi kontrol et
+
+      return (
+        <div key={index}>
+          <motion.div
+            className={`w-full menu text-black ${
+              isLastItem ? "border-b-0" : "border-b-2"
+            } pb-2 text-[13px] border-[#3C3C43]/10`} // Son öğe için border-b-0 ekle
+            onClick={(e) => {
+              if (item.menu) {
+                if (activeMenuPath.includes(currentPath)) {
+                  setActiveMenuPath((prev) =>
+                    prev.filter((path) => path !== currentPath)
+                  );
+                } else {
+                  setActiveMenuPath((prev) => [...prev, currentPath]);
+                }
+              }
+            }}
+          >
+            {item.title}
+          </motion.div>
+
+          <AnimatePresence key={item.title}>
+            {item.menu && activeMenuPath.includes(currentPath) && (
+              <motion.div
+                initial="initial"
+                animate="animate"
+                exit="exit"
+                variants={menuAnimation}
+                className="absolute menu max-w-56 ml-2 w-screen left-full top-0 bg-white/60 backdrop-blur-md border border-black/10 z-10 p-2.5 drop-shadow-sm flex flex-col gap-2.5 rounded-md"
+              >
+                {renderMenuItems(item.menu, [...path, item.title])}
+              </motion.div>
+            )}
+          </AnimatePresence>
+        </div>
+      );
+    });
+  };
+
+  const menuItems: {
+    title: string;
+    menu: boolean | object[];
+    appId?: number;
+  }[] = [
+    {
+      title: "Finder",
+      menu: false,
+      appId: 1,
+    },
+    {
+      title: "Edit",
+      menu: false,
+    },
+    {
+      title: "View",
+      menu: false,
+    },
+    {
+      title: "Go",
+      menu: false,
+    },
+    {
+      title: "Window",
+      menu: [
+        {
+          title: "Edit 1",
+          menu: false,
+        },
+        {
+          title: "Edit 2",
+          menu: false,
+        },
+        {
+          title: "Edit 3",
+          menu: [
+            {
+              title: "Edit 3.1",
+              menu: false,
+            },
+            {
+              title: "Edit 3.2",
+              menu: false,
+            },
+          ],
+        },
+      ],
+    },
+    {
+      title: "Help",
+      menu: false,
+    },
+  ];
+
+  const apps: number[] = [1];
+
   return (
     <div className="w-full shadow-sm py-1 shadow-[#8F8F8F]/10 flex items-center justify-between px-4 backdrop-blur-lg bg-white/20">
-      <div className="flex items-center h-full gap-4">
+      <div className="flex items-center h-full">
         <div
-          ref={appleLogoRef}
           onClick={(e) => {
-            e.stopPropagation();
-            setOpen(true);
+            setOpen(!open);
           }}
-          className={`relative px-[12px] py-[2px] transition-all rounded duration-200 ease-in-out ${
+          className={`relative menu px-[12px] py-[2px] transition-all rounded duration-200 ease-in-out ${
             open ? "bg-white/20" : "bg-white/0 hover:bg-white/10"
           }`}
         >
@@ -61,12 +153,11 @@ const Navbar: React.FC = () => {
           <AnimatePresence>
             {open && (
               <motion.div
-                ref={menuRef}
                 initial="initial"
                 animate="animate"
                 exit="exit"
                 variants={menuAnimation}
-                className="absolute max-w-56 mt-2 w-screen left-0 top-full bg-white/60 backdrop-blur-md border border-black/10 z-10 p-2.5 drop-shadow-sm flex flex-col gap-2.5 rounded-md"
+                className="absolute menu max-w-56 mt-2 w-screen left-0 top-full bg-white/60 backdrop-blur-md border border-black/10 z-10 p-2.5 drop-shadow-sm flex flex-col gap-2.5 rounded-md"
               >
                 <motion.div className="w-full border-b-2 pb-2 text-[13px] border-[#3C3C43]/10">
                   About This Mac
@@ -96,13 +187,56 @@ const Navbar: React.FC = () => {
           </AnimatePresence>
         </div>
 
-        <span className="text-sm font-medium text-white ml-1">Finder</span>
-        <span className="text-sm text-white">File</span>
-        <span className="text-sm text-white">Edit</span>
-        <span className="text-sm text-white">View</span>
-        <span className="text-sm text-white">Go</span>
-        <span className="text-sm text-white">Window</span>
-        <span className="text-sm text-white">Help</span>
+        {menuItems.map((menu, index) => {
+          if (!menu?.menu)
+            return (
+              <span
+                key={index}
+                className={`${
+                  menu.appId && apps.includes(menu?.appId)
+                    ? "font-medium"
+                    : "font-normal"
+                } text-sm text-white cursor-pointer hover:bg-white/20 px-3 py-1 rounded-md`}
+              >
+                {menu.title}
+              </span>
+            );
+
+          if (menu?.menu)
+            return (
+              <div key={index} className="relative text-sm text-white">
+                <span
+                  className="cursor-pointer menu hover:bg-white/20 px-3 py-1 rounded-md"
+                  onClick={(e) => {
+                    const currentPath = menu.title;
+                    if (activeMenuPath.includes(currentPath)) {
+                      setActiveMenuPath((prev) =>
+                        prev.filter((path) => path !== currentPath)
+                      );
+                    } else {
+                      setActiveMenuPath([currentPath]);
+                    }
+                  }}
+                >
+                  {menu.title}
+                </span>
+
+                <AnimatePresence>
+                  {activeMenuPath.includes(menu.title) && (
+                    <motion.div
+                      initial="initial"
+                      animate="animate"
+                      exit="exit"
+                      variants={menuAnimation}
+                      className="absolute menu max-w-56 mt-2.5 w-screen left-0 top-full bg-white/60 backdrop-blur-md border border-black/10 z-10 p-2.5 drop-shadow-sm flex flex-col gap-2.5 rounded-md"
+                    >
+                      {renderMenuItems(menu.menu as object[], [menu.title])}
+                    </motion.div>
+                  )}
+                </AnimatePresence>
+              </div>
+            );
+        })}
       </div>
 
       <div className="flex items-center h-full gap-1">
